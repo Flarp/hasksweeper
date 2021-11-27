ac12803edc38fdab3f199804d257036d98e8a74d
Flarp <flurpdadurp@gmail.com>
2021-11-26 21:53:48 -0500

    not adding autocomplete so this is final version

--- src/Structure.hs
+++ src/Structure.hs
@@ -1,7 +1,8 @@
 module Structure where
 
 import Data.Matrix hiding (trace)
-import Data.Set hiding (map, foldl, filter, toList)
+import Data.Set hiding (map, foldl, filter, toList, delete)
+import Data.List hiding (insert)
 import Data.Maybe
 import Data.Ix
 import Debug.Trace
@@ -9,7 +10,6 @@ import Control.Applicative hiding (empty)
 
 gridSize = 18 :: Int
 bombCount = 40
-corners = [(-1,0), (0,-1), (0,1), (1,0)]
 
 -- A square can either be covered but unflagged, covered and flagged,
 -- or uncovered (in which the number of surrounding bombs is visible)
@@ -48,7 +48,9 @@ testForBomb :: Int -> Int -> GameMatrix -> Bool
 testForBomb i j mat = bomb $ getElem i j mat
 
 hasWon :: GameMatrix -> Bool
-hasWon mat = (foldl (\prev curr -> prev + (fromEnum $ not (isUncovered $ coverage curr))) 0 (toList mat)) == bombCount
+hasWon mat = (foldl (\prev curr ->
+                      prev + (fromEnum $ not (isUncovered $ coverage curr)))
+              0 (toList mat)) == bombCount
 
 hasLost :: GameMatrix -> Bool
 hasLost mat = any (liftA2 (&&) (isUncovered . coverage) bomb) (toList mat)
@@ -74,27 +76,33 @@ computeSurrounding i j mat =
 -- Recursively uncovers squares from a given starting location, provided a coordinate location,
 -- a game matrix, and a set of previously searched coordinates. The algorithm searches in a star
 -- formation, checking each north, west, south, and east square recursively and stopping if this
--- square has already been uncovered or a bomb has been detected in an adjacent square.
+-- square has already been uncovered or a bomb has been detected in an adjacent square. If we are
+-- uncovering a square that has already been uncovered, we should uncover the 8 adjacent squares instead.
 uncoverSquare :: (Int, Int) -> GameMatrix -> Set (Int, Int) -> (GameMatrix, Set (Int, Int))
 uncoverSquare (i, j) mat discovered = do
   let center = getElem i j mat
   let bomp = bomb center
   let square = Square {
     bomb = bomp,
-    coverage = if (bomp && (discovered /= empty)) then (coverage center) else Uncovered $ computeSurrounding i j mat
+    coverage = if (bomp && (discovered /= empty))
+                then (coverage center)
+                else Uncovered $ computeSurrounding i j mat
   }
   let new = setElem square (i, j) mat
   let newDiscover = insert (i, j) discovered
 
-  if ((isntUncoverable center) || ((getSurrounding $ coverage square) /= 0)) then (new, empty) else (fst $ foldl (\(pmat, pset) (x, y) -> uncoverSquare (i+x, j+y) pmat pset)
-    (new, newDiscover)
-    (filter (\(x, y) ->
-      and [
-        (not $ isUncovered $ coverage center),
-        (isJust $ safeGet (i+x) (j+y) new),
-        (not $ member (i+x, j+y) newDiscover)
-      ])
-    corners), empty)
+  let filterand = case (coverage center) of
+                    Uncovered sur -> range ((-1, -1), (1, 1))
+                    _ -> [(-1,0), (0,-1), (0,1), (1,0)]
 
---main :: IO ()
---main = putStrLn "shart"
+  let array = filter (\(x, y) -> and [
+                --((not $ isUncovered $ coverage center) || (discovered == empty)),
+                (isJust $ safeGet (i+x) (j+y) new),
+                (not $ member (i+x, j+y) newDiscover)
+              ]) filterand
+
+  if (not $ isntUncoverable center) && ((getSurrounding $ coverage square) == 0)
+    then
+      (fst $ foldl (\(pmat, pset) (x, y) -> uncoverSquare (i+x, j+y) pmat pset)
+        (new, newDiscover) array, empty)
+    else (new, empty)
